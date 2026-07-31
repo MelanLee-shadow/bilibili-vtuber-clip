f6e0754dde07627b70f24358ef8494642b2b0219
aierlma <aierlma521@gmail.com>
2026-07-30 23:27:19 -0400

    fix(titles): fit July 26 source title gate

--- tests/test_title_policy.py
+++ tests/test_title_policy.py
@@ -107,7 +107,7 @@ def test_automatic_title_filler_canonicalizer_only_removes_profile_exact_words()
         ),
         (
             "auto_145940_1533_1619r3",
-            "先嘲笑爱音上不起“月之森贵族学校”，连声道歉称忘了她是英国留子，又猜她回国可能因没办法自己做饭",
+            "嘲笑爱音上不起“月之森贵族学校”，道歉说忘了她是英国留子，又猜她回国可能因为不会做饭",
         ),
         (
             "auto_152944_964_1091",
